cc2a387c3860388e65c5a546cb5f6cac665f303e
macavilla <maca.villa.a@gmail.com>
2025-07-09 23:55:37 -0300

    feat(Home): integrate pagination with searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,48 @@
 import { CharacterType } from "@/types/character";
+import { PaginationType } from "@/types/pagination";
 
 import Dashboard from "@/components/Dashboard/Dashboard";
+import Pagination from "@/components/Pagination/Pagination";
+import Link from "next/link";
 
-const getResponse = async (): Promise<{
+interface HomeProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+const getResponse = async (
+  page: string | string[] | undefined
+): Promise<{
   allCharacters: CharacterType[];
-  paginationInfo: object;
+  paginationInfo: PaginationType;
 }> => {
-  const res = await fetch(`https://rickandmortyapi.com/api/character`);
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page && page}`
+  );
   const response = await res.json();
 
   return { allCharacters: response.results, paginationInfo: response.info };
 };
-export default async function Home() {
-  const { allCharacters } = await getResponse();
+export default async function Home({ searchParams }: HomeProps) {
+  const page = (await searchParams).page;
 
-  // console.log("characters :>> ", characters);
+  const { allCharacters, paginationInfo } = await getResponse(page);
 
   return (
     <div className="min-h-screen p-4 sm:p-6 font-[family-name:var(--font-geist-sans)]">
-      <main className="">
+      <main>
         <h1 className="text-3xl font-bold">Rick and Morty Characters</h1>
 
-        <Dashboard characters={allCharacters} />
+        {paginationInfo && (
+          <Pagination paginationInfo={{ ...paginationInfo }} />
+        )}
+        {allCharacters ? (
+          <Dashboard characters={allCharacters} />
+        ) : (
+          <div className="min-h-screen flex flex-col justify-center items-center">
+            <h2 className="text-2xl">Characters not found</h2>
+            <Link href="/">Try again</Link>
+          </div>
+        )}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
