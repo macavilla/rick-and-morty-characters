7b127638f1e366d1ab1757ae82bc6483a9d50d72
macavilla <maca.villa.a@gmail.com>
2025-07-10 00:20:32 -0300

    fix(CharacterList): add opt chainig

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -21,7 +21,7 @@ const CharacterList = ({
       <h3 className="text-2xl">Character #{listId}</h3>
 
       <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4 border-4">
-        {characters.map((character: CharacterType) => (
+        {characters?.map((character: CharacterType) => (
           <CharacterCard
             key={character.id}
             character={character}
