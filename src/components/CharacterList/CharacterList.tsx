4f5e72b2aae90ca338b37a55968af5a32022c431
macavilla <maca.villa.a@gmail.com>
2025-07-10 20:17:51 -0300

    feat: add styles and layout

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -20,7 +20,7 @@ const CharacterList = ({
     <div className="">
       <h3 className="text-2xl">Character #{listId}</h3>
 
-      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4 border-4">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2 border-3 rounded-4xl p-2 my-2">
         {characters?.map((character: CharacterType) => (
           <CharacterCard
             key={character.id}
