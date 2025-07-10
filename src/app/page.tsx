e63f6bea751c179d649e759c705bc53eb7613580
macavilla <maca.villa.a@gmail.com>
2025-07-10 20:17:23 -0300

    feat: layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,22 +27,16 @@ export default async function Home({ searchParams }: HomeProps) {
   const { allCharacters, paginationInfo } = await getResponse(page);
 
   return (
-    <div className="min-h-screen p-4 sm:p-6 font-[family-name:var(--font-geist-sans)]">
-      <main>
-        <h1 className="text-3xl font-bold">Rick and Morty Characters</h1>
-
-        {paginationInfo && (
-          <Pagination paginationInfo={{ ...paginationInfo }} />
-        )}
-        {allCharacters ? (
-          <Dashboard characters={allCharacters} />
-        ) : (
-          <div className="min-h-screen flex flex-col justify-center items-center">
-            <h2 className="text-2xl">Characters not found</h2>
-            <Link href="/">Try again</Link>
-          </div>
-        )}
-      </main>
-    </div>
+    <>
+      {paginationInfo && <Pagination paginationInfo={{ ...paginationInfo }} />}
+      {allCharacters ? (
+        <Dashboard characters={allCharacters} />
+      ) : (
+        <div className="min-h-screen flex flex-col justify-center items-center">
+          <h2 className="text-2xl">Characters not found</h2>
+          <Link href="/">Try again</Link>
+        </div>
+      )}
+    </>
   );
 }
