b515ce43df78d778abdfb4ea0fc27503744c6d28
macavilla <maca.villa.a@gmail.com>
2025-07-11 00:19:15 -0300

    test(EpisodesList): add test, update mock

--- src/components/Episodes/EpisodesList/EpisodesList.test.tsx
+++ src/components/Episodes/EpisodesList/EpisodesList.test.tsx
@@ -12,4 +12,20 @@ describe("EpisodesList", () => {
   it("should render three EpisodesListList with shared episodes data", () => {
     render(<EpisodesList {...mockEpisodesListProps} />);
   });
+
+  it("renders list of episodes", () => {
+    render(<EpisodesList {...mockEpisodesListProps} />);
+    expect(
+      screen.getByText("S01E01 - Pilot - December 2, 2013")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("S01E02 - Lawnmower Dog - December 9, 2013")
+    ).toBeInTheDocument();
+  });
+
+  it("does not render list when episodes is null", () => {
+    render(<EpisodesList {...mockEpisodesListProps} episodes={null} />);
+    const list = screen.queryByRole("list");
+    expect(list).not.toBeInTheDocument();
+  });
 });
